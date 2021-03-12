1077154084c072074b588350f4cd2e76351c230c
tokseek <toksee@yandex.ru>
2021-03-13 01:13:51 +0300

    feat: сделать открытие/закрытие popup на js по клику

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+const popup = document.querySelector('.popup');
+
+function OpenPopup () {
+    popup.classList.add('popup_opened');
+}
+
+function ClosePopup () {
+    popup.classList.remove('popup_opened');
+}
