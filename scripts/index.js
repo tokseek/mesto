67b779695cc99b873e35735396da300f6a8392ac
tokseek <toksee@yandex.ru>
2021-03-15 01:31:05 +0300

    feat: Разобраться с Submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 const popup = document.querySelector('.popup');
+const openPopupBtn = document.querySelector('.profile__edit-button');
+const closePopupBtn = document.querySelector('.popup__close');
+const closePopupOverlay = document.querySelector('.popup__overlay');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
+const inputName = document.querySelector('.popup__input-name');
+const inputStatus = document.querySelector('.popup__input-status');
 
-function OpenPopup () {
+
+function OpenPopup() {
     popup.classList.add('popup_opened');
+    inputName.value = profileName.textContent;
+    inputStatus.value = profileStatus.textContent;
 }
 
-function ClosePopup () {
+function ClosePopup() {
     popup.classList.remove('popup_opened');
 }
+
+openPopupBtn.addEventListener('click', function () {
+    OpenPopup();
+});
+
+closePopupBtn.addEventListener('click', function () {
+    ClosePopup();
+});
+closePopupOverlay.addEventListener('click', function () {
+    ClosePopup();
+});
+
+const saveBtn = document.querySelector('.popup__button');
+
+
+
