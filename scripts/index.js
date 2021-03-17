08165cb5709fac95a5ae294de1bbba7acf094c29
Danila Mitin <tokseek@yandex.ru>
2021-03-17 16:47:35 +0300

    fix: исправить все замечания ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,11 @@
-const popup = document.querySelector('.popup');
-const openPopupBtn = document.querySelector('.profile__edit-button');
-const closePopupBtn = document.querySelector('.popup__close');
-const closePopupOverlay = document.querySelector('.popup__overlay');
-const profileName = document.querySelector('.profile__name');
-const profileStatus = document.querySelector('.profile__status');
-const inputName = document.querySelector('.popup__input-name');
-const inputStatus = document.querySelector('.popup__input-status');
-const formSubmit = document.querySelector('.popup__form');
+let popup = document.querySelector('.popup');
+let openPopupBtn = document.querySelector('.profile__edit-button');
+let closePopupBtn = document.querySelector('.popup__close');
+let profileName = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status');
+let inputName = document.querySelector('#input-name');
+let inputStatus = document.querySelector('#input-status');
+let formSubmit = document.querySelector('.popup__form');
 
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -18,18 +17,6 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-openPopupBtn.addEventListener('click', function () {
-    openPopup();
-});
-
-closePopupBtn.addEventListener('click', function () {
-    closePopup();
-});
-closePopupOverlay.addEventListener('click', function () {
-    closePopup();
-});
-
-
 function changeValue(event) {
     event.preventDefault();
     profileName.textContent = inputName.value;
@@ -38,3 +25,5 @@ function changeValue(event) {
 }
 
 formSubmit.addEventListener('submit', changeValue);
+openPopupBtn.addEventListener('click', openPopup);
+closePopupBtn.addEventListener('click', closePopup);
