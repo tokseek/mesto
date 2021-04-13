94b8917e9060c9fc4152ac6d5b0304087973702e
tokseek <toksee@yandex.ru>
2021-04-13 22:52:32 +0300

    feat: rуd-underline

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,13 +1,17 @@
 const showInputError = (formElement, inputElement, errorMessage) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    const errorUnderline = formElement.querySelector('.popup__input');
+    errorUnderline.classList.add("popup__input-error-underline");
     errorElement.textContent = errorMessage;
     errorElement.classList.add("popup__input-error_active");
 };
 
 const hideInputError = (formElement, inputElement) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    const errorUnderline = formElement.querySelector('.popup__input');
     errorElement.textContent = '';
     errorElement.classList.remove("popup__input-error_active");
+    errorUnderline.classList.remove("popup__input-error-underline");
 };
 
 const checkInputValidity = (formElement, inputElement) => {
@@ -21,19 +25,36 @@ const checkInputValidity = (formElement, inputElement) => {
     }
 } 
 
+const toggleButtonState = (inputList, buttonElement) => {
+    const hasNotValidInput = inputList.some(
+        (inputElement) => !inputElement.validity.valid
+    );
+
+    if (hasNotValidInput) {
+        buttonElement.setAttribute('disabled', true);
+        buttonElement.classList.add("popup__submit_inactive");
+    } else {
+        buttonElement.removeAttribute('disabled');
+        buttonElement.classList.remove("popup__submit_inactive");
+    }
+}
+
 const setEventListeners = (formElement) => {
     formElement.addEventListener('submit', event => {
         event.preventDefault();
     });
 
-    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+    const buttonElement = formElement.querySelector(".popup__submit");
 
-    inputList.forEach(inputElement => {
-        inputElement.addEventListener("input", (event) => {
-            checkInputValidity(formElement, inputElement);
-        });
+inputList.forEach(inputElement => {
+    inputElement.addEventListener("input", (event) => {
+        checkInputValidity(formElement, inputElement);
+        toggleButtonState(inputList, buttonElement);
     });
-
+});
+// сделаем чтобы кнопка была неактивна сразу при инициализации
+    toggleButtonState(inputList, buttonElement);   
 };
 
 const enableValidation = () => {
